5f10b6de789c9f632a6677777a3e166e11b1bf25
Xiaowen Feng <xfeng1@uw.edu>
2015-10-15 15:29:57 -0700

    in class code

--- javascript/js/states.js
+++ javascript/js/states.js
@@ -1,3 +1,35 @@
 /**
  * application script for the states.html
  */
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+
+    function addStates(states, listElem) {
+        listElem.innerHTML = '';
+        states.forEach(function(state) {
+
+           //console.log(state);
+            var li = document.createElement('li');
+            li.textContent = state.name;
+            listElem.appendChild(li);
+
+        });
+    }
+
+    var statesFilter = document.getElementById('state-filter-field');
+    statesFilter.addEventListener('keyup', function () {
+        var filter = this.value.toLocaleLowerCase();
+       // console.log(filter);
+        var filteredStates = usaStates.filter(function(state) {
+            return state.name.toLowerCase().indexOf(filter) >= 0;
+
+        });
+
+        addStates(filteredStates, statesUl);
+    });
+
+    var statesUl = document.getElementById('states-list');
+    addStates(usaStates, statesUl);
+});
